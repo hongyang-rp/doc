4ce2725d47e7e5fba6d98e9e4e0b25cf50abbf3c
hongyang-rp <hongyang.zhao@thundersoft.com>
2025-05-05 20:07:35 +0800

    first commit
    Signed-off-by: hongyang-rp <hongyang.zhao@thundersoft.com>

---
+++ source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'test'
+copyright = '2025, hongyang zhao'
+author = 'hongyang zhao'
+release = 'n'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'cn'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
